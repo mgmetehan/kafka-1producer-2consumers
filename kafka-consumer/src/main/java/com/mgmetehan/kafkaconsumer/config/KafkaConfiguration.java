47fd3d7d1602af2c1910b5e4d38bd2c7e93fec7b
Metehan Gultekin <mgmetehan@hotmail.com>
2022-07-21 11:59:02 +0300

    Add Kafka Consumer Project

---
+++ kafka-consumer/src/main/java/com/mgmetehan/kafkaconsumer/config/KafkaConfiguration.java
@@ -0,0 +1,41 @@
+package com.mgmetehan.kafkaconsumer.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.mgmetehan.kafkaconsumer.dto.KMessage;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+@Configuration
+public class KafkaConfiguration {
+    @Value("${mg.kafka.address}")
+    private String kafkaAddress;
+    @Value("${mg.kafka.group.id}")
+    private String groupId;
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, KMessage> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, KMessage> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, KMessage> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaAddress);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, KMessage.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+}
